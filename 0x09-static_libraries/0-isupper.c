f10bf2c38257ecea96a0dbd04ab622caa72cc228
ShalomSuccess <shalomsuccess123@gmail.com>
2023-03-10 09:26:03 -0800

    update

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase
- * @c: character to checked
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
  */
 int _isupper(int c)
 {
