7156b427a7e5c45e4136825d44c6361864e1831b
ShalomSuccess <shalomsuccess123@gmail.com>
2023-03-13 06:02:38 -0700

    message

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,6 +4,12 @@ char *create_array(unsigned int size, char c){
 	char *ptArray;
 	unsigned int count;
 	ptArray = malloc(size * sizeof(char));
+
+
+	if (size == 0 || str == NULL)
+
+		return (NULL);
+
 	for(count = 0; count < size; count++){
 		ptArray[count] = c;
 	}
