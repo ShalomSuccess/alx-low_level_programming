f7fffca1b38d9d7ca5fe5b16de19c4bc75f56fca
ShalomSuccess <shalomsuccess123@gmail.com>
2023-04-09 11:55:47 -0700

    message

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX stdout
- * @filename: pointer to name of file to read
- * @letters: number of letters to read and print
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
  *
- * Return: actual number of letters read and printed; or 0 on failure
+ * Return: actual number of letters it could read and print
  */
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (filename == NULL) {
-        return 0;
-    }
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int sha;
+	ssize_t lenr, len;
+	char *buffer;
 
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
-        return 0;
-    }
-
-    char *buffer = (char *) malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL) {
-        fclose(fp);
-        return 0;
-    }
-
-    size_t bytes_read = fread(buffer, sizeof(char), letters, fp);
-    if (bytes_read == 0) {
-        fclose(fp);
-        free(buffer);
-        return 0;
-    }
-
-    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read) {
-        fclose(fp);
-        free(buffer);
-        return 0;
-    }
-
-    fclose(fp);
-    free(buffer);
-    return bytes_written;
+	if (filename == NULL)
+		return (0);
+	sha = open(filename, O_RDONLY);
+	if (sha == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(sha);
+		return (0);
+	}
+	lenr = read(sha, buffer, letters);
+	close(sha);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	len = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != len)
+		return (0);
+	return (len);
 }
