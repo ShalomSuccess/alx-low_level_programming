59cc22e7eca5b7a6b9acf18bdfe961948a654dc0
ShalomSuccess <shalomsuccess123@gmail.com>
2023-04-10 06:33:55 -0700

    message

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,10 +10,14 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8) {
-        return -1;
+	int bit_value;
+    if (index > 63) {
+        return (-1);
     }
 
-    unsigned long int mask = 1UL << index;
-    return (n & mask) != 0;
+	bit_value = (n >> index) & 1;
+
+	return (bit_value);
 }
+
+
